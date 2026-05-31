723834d107495564e82e71191b675bbc8330d44a
fitzz <FitzzTeXnikWelt@t-online.de>
2026-05-31 02:22:49 +0200

    refactor(ca-root-signer): adapt to streamlined baselibs and achieve 100% coverage
    - Remove deprecated configuration references and legacy DEV hooks
    - Update internal module dependencies to align with direct TOML parsing
    - Ensure 100% code coverage stability across all signing components

--- doc/source/devel/get_started_run_programms.ci.rst
+++ doc/source/devel/get_started_run_programms.ci.rst
@@ -1,115 +1,127 @@
-Running the programm Successfully and Errors
-==============================================
+The Signing Programm
+######################
 
 .. SECTION - Setup
 
+>>> test_data_pre= "test_ok_data2"
 >>> from fitzzftw.devtools.testinfra import TestHomeEnvironment
 >>> from pathlib import Path
->>> env = TestHomeEnvironment(Path("doc/source/devel/testhome"))
+>>> env = TestHomeEnvironment(Path("doc/source/devel/testhome"),
+...     appname="ftwpki", appauthor="FitzzTeXnikWelt")
 >>> env.setup(True)
+>>> env.clean_home()
+>>> env.clean_output()
 
 .. !SECTION
 .. SECTION - Prepare
 
->>> from pathlib import Path
+>>> ca_pki_path = env.copy2cwd(f"{test_data_pre}/ca_root.pki",
+...             "ca_root.pki")
 
->>> _ = env.copy2cwd("ca_root_conf.toml")
->>> Path("privat").mkdir(parents=True, exist_ok=True)
->>> _ = env.copy2cwd("privat/testpasswd")
->>> _ = env.copy2cwd("privat/ca.key.pem")
->>> _ = env.copy2cwd("ca_public/ca.cert", "ca.cert")
->>> _ = env.copy2cwd("Fitzz-TeXnik-WeltSomewherecity.csr")
+>>> cert_path = env.copy2cwd(f"{test_data_pre}/Muster-Verband-Hamburg-Regional-CA_Hamburg.csr",
+...             "M-V-HH-CA_Hamburg.csr")
 
->>> def stub_input(prompt:str):
+>>> cert_name = cert_path.name
+
+
+>>> def getpasswd(prompt:str)->str:
 ...     print(prompt)
-...     return "strenggeheim"
+...     return "secret"
+
+>>> def stub_keyboard_interrupt(prompt:str):
+...     raise KeyboardInterrupt
+
+>>> def stub_exception(prompt:str):
+...     raise Exception("Test exception!")
 
 >>> import getpass
->>> getpass.getpass = stub_input
+>>> getpass.getpass = getpasswd
 
->>> cmd_line =  "--conf-file ca_root_conf.toml"
->>> cmd_line += " -k privat/ca "
->>> cmd_line += " --private-dir privat"
->>> cmd_line += " --policy-name intermediate"
->>> cmd_line += " -c ca.cert"
->>> cmd_line += " testpasswd"
->>> cmd_line += " Fitzz-TeXnik-WeltSomewherecity.csr"
+
+>>> cmd_line = " --policy-name intermediate"
+>>> cmd_line += " -c ca_root.pki "
+>>> cmd_line += " carootsecret "
+>>> cmd_line += f" {cert_name} "
 
 >>> import shlex
 >>> sys_argv= shlex.split(cmd_line) 
 >>> sys_argv #doctest: +NORMALIZE_WHITESPACE
-['--conf-file', 'ca_root_conf.toml', 
- '-k', 'privat/ca', 
- '--private-dir', 'privat', 
- '--policy-name', 'intermediate',
- '-c', 'ca.cert',
- 'testpasswd',
- 'Fitzz-TeXnik-WeltSomewherecity.csr']
-
+['--policy-name', 'intermediate', 
+ '-c', 'ca_root.pki', 
+ 'carootsecret', 
+ 'M-V-HH-CA_Hamburg.csr']
 
 .. !SECTION
-.. SECTION - Start programm function
+
+>>> from ftwpki.baselibs.utils import print_error
+
+>>> import ftwpki.ca_root_signer.programms
+
+>>> ftwpki.ca_root_signer.programms.report_error = print_error
 
 >>> from ftwpki.ca_root_signer.programms import prog_ca_root_signing
 
+>>> db_path = Path("db/index.txt")
+
+>>> db_path.is_file()
+False
+
 >>> prog_ca_root_signing(sys_argv)
 Enter Password:
 0
 
-..SECTION dn missmatch
+>>> db_path.is_file()
+True
+
+>>> db_path.read_text() #doctest: +ELLIPSIS
+'V\t...\t\t...\t...\tCN=Muster-Verband Hamburg Regional CA...'
+
+>>> prog_ca_root_signing(sys_argv)
+Enter Password:
+0
+
+>>> print(db_path.read_text()) #doctest: +ELLIPSIS +NORMALIZE_WHITESPACE
+V   ...           ...        ... CN=Muster-Verband Hamburg Regional CA...
+V   ...           ...        ... CN=Muster-Verband Hamburg Regional CA...
+<BLANKLINE>
 
 
 
+>>> cmd_line = " --policy-name intermediate "
+>>> cmd_line += " -c ca_root.pki "
+>>> cmd_line += " -CN no "
+>>> cmd_line += " -OU no "
+>>> cmd_line += " carootsecret "
+>>> cmd_line += f" {cert_name} "
 
-
-..!SECTION dn missmatch
-
->>> cmd_line =  "--conf-file ca_root_conf.toml"
->>> cmd_line += " -k privat/ca.key.pem "
->>> cmd_line += " --private-dir privat"
->>> cmd_line += " --policy-name intermediate"
->>> cmd_line += " -c ca.cert"
->>> cmd_line += " -ST no"
->>> cmd_line += " testpasswd"
->>> cmd_line += " Fitzz-TeXnik-WeltSomewherecity.csr"
 >>> sys_argv= shlex.split(cmd_line) 
->>> prog_ca_root_signing(sys_argv) #doctest: +NORMALIZE_WHITESPACE
-  - [stateOrProvinceName]: DISALLOWED
+
+>>> prog_ca_root_signing(sys_argv) 
+While policyvalidation following missmatch occurs:
+    - [commonName]: DISALLOWED
+    - [organizationalUnitName]: DISALLOWED
 1
 
->>> cmd_line =  "--conf-file ca_root_conf.toml"
->>> cmd_line += " -k privat/ca.key.pem "
->>> cmd_line += " --private-dir privat"
->>> cmd_line += " --policy-name intermediate"
->>> cmd_line += " -c ca.cert"
->>> cmd_line += " -ST MACH"
->>> cmd_line += " testpasswd"
->>> cmd_line += " Fitzz-TeXnik-WeltSomewherecity.csr"
->>> sys_argv= shlex.split(cmd_line) 
->>> prog_ca_root_signing(sys_argv) #doctest: +NORMALIZE_WHITESPACE +ELLIPSIS
-argument -ST/--stateOrProvinceName: invalid choice: 'MACH' (choose from ...)
-2
 
-.. SECTION - Template
+>>> cmd_line = " --policy-name intermediate"
+>>> cmd_line += " -c ca_root.pki "
+>>> cmd_line += " carootsecret "
+>>> cmd_line += f" {cert_name} "
 
->>> cmd_line =  "--conf-file ca_root_conf.toml"
->>> cmd_line += " -k privat/ca "
->>> cmd_line += " --private-dir privat"
->>> cmd_line += " --policy-name intermediate"
->>> cmd_line += " -c ca.cert"
->>> cmd_line += " testpasswd"
->>> cmd_line += " Fitzz-TeXnik-WeltSomewherecity.csr"
 >>> sys_argv= shlex.split(cmd_line) 
+>>> getpass.getpass=stub_keyboard_interrupt
 >>> prog_ca_root_signing(sys_argv)
-Enter Password:
-0
+1
 
+>>> getpass.getpass=stub_exception
+>>> prog_ca_root_signing(sys_argv)
+Test exception!
+1
 
-.. !SECTION - Template
 
 .. SECTION - Teardown
 
->>> env.clean_home()
+>> env.clean_home()
 >>> env.teardown()
 
 .. !SECTION
--- src/ftwpki/ca_root_signer/programms.py
+++ src/ftwpki/ca_root_signer/programms.py
@@ -18,7 +18,7 @@ from ftwpki.baselibs.cli_parser import (
     CSRSigningParser,
     TomlPreParser,
 )
-from ftwpki.baselibs.configuration import PKIPackage, RootSignerPKIConfig_DEV
+from ftwpki.baselibs.configuration import PKIPackage, RootSignerPKIConfig
 from ftwpki.baselibs.core import (
     cert_to_record,
     get_subject_dict,
@@ -37,110 +37,17 @@ from ftwpki.baselibs.transport import encrypt_transport_package
 from ftwpki.baselibs.utils import report_error
 from ftwpki.baselibs.validate import ValidatorDN, validate_and_clamp_validity
 
+# FIXME - Programm Signing
 
-# SECTION - Programm Signing
-def prog_ca_root_signing(argv: list[str] | None = None) -> int:
-    """
-    Entry point for signing Certificate Signing Requests (CSRs). (rw)
-
-    Validates the CSR against the CA policy, signs it using the Root-CA key,
-    and prepares the transport package.
-
-    :param argv: Optional list of command-line arguments.
-    :returns: Exit code (0 for success, 1 for validation error, 2 for other errors).
-    """
-    try:
-        # SECTION - Configuration
-        pre_parser = TomlPreParser()
-        pre_args, _ = pre_parser.parse_known_args(argv)
-        ca_parser = CSRSigningParser()
-        # for k, v in toml2_dn_policy(argv).items():
-        #     print(f"{k}: {v}")
-        file_conf = toml2dn_policy(pre_args.conf_file, pre_args.policy_name) 
-        ca_parser.set_defaults(**file_conf)
-        # ca_parser.set_defaults(**toml2_dn_policy(argv))
-        extention = toml2ext(pre_args.conf_file, pre_args.policy_name)
-        args = ca_parser.parse_args(argv)
-        # !SECTION - Configuration
-
-        # print(f"{args.policy}")
-        # SECTION - Validating
-        ca_cert = load_certificate_from_pem(pem_data=Path(args.certificate).read_bytes())
-        csr = load_csr_from_pem(Path(args.certificat_sign_request).read_bytes())
-        val_dn = ValidatorDN(args.policy, get_subject_dict(ca_cert))
-        validate_result = val_dn.validate(get_subject_dict(csr),raise_on_error=False)
-        if not validate_result.is_valid:
-            for error in validate_result.errors:
-                print(error)
-            return 1
-        # !SECTION - Validating
-
-        # SECTION - Passwordhandling
-        pwd_man = PasswordManager(private_dir=args.private_dir)
-        pass_phrase = pwd_man.decrypt_password_file(
-            args.passphrasefile, password=getpass.getpass("Enter Password:")
-        )
-        # !SECTION - Passwordhandling
-
-        # SECTION - Signing
-        private_key_obj = load_private_key_from_pem(
-            pem_data=Path(args.private_key).read_bytes(), passphrase=pass_phrase
-        )
-        cert_signer = CertificateSigner(ca_cert=ca_cert, ca_key=private_key_obj)
-        policy = IntermediatePolicy(path_length=args.path_length)
-        validity_days = validate_and_clamp_validity(ca_cert, args.validity_days)
-        signed_cert = cert_signer.sign(
-            csr=csr, policy=policy, validity_days=validity_days.actual_days, **extention
-        )
-        signed_pem = cert_signer.get_pem(signed_cert)
-        target_path:Path = Path(args.certificat_sign_request).with_suffix(".crt.pem")
-        save_pem(
-            data=signed_pem,
-            target_path= target_path,
-            is_private=True,
-        )
-        # !SECTION - Signing
-
-        # SECTION - Transferfile
-        zipped_data = encrypt_transport_package(
-            signed_cert,  # user_cert
-            ca_cert,  # root_ca_cert
-            private_key_obj,
-            signed_cert,  # recipient_cert
-            signed_cert,
-            ca_cert,
-            name_user=target_path.name,
-            name_chain="all.chain.pem",
-            name_ca = "ca.crt.pem",
-        )
-
-        transfer_file_path = Path(args.certificat_sign_request).with_suffix(".zip.enc")
-        transfer_file_path.write_bytes(zipped_data)
-        # !SECTION - Transferfile
-
-        # SECTION - Database openssl compatible
-        db_dir = Path("db")
-        if not db_dir.is_dir():
-            db_dir.mkdir(parents=True)
-        db_file = DbOpensslFile(db_dir / "index.txt")
-        db_file.add_record(
-            record=cert_to_record(cert=load_certificate_from_pem(signed_pem), status="V")
-        )
-        # !SECTION - Database openssl compatible
-        return 0
-    except Exception as e:
-        print(e)
-        return 2
-# !SECTION - Programm Signing
-
-#SECTION - prog_ca_root_signing_DEV
+#SECTION - prog_ca_root_signing
 # DOC - new
-def prog_ca_root_signing_DEV(argv: list[str] | None = None) -> int:
+def prog_ca_root_signing(argv: list[str] | None = None) -> int:
     try:
+        temp_key_pem = "caroot.key.pem"
         # SECTION - Configuration
         pre_parser = CSRSigningParser(add_help=False, allow_abbrev=False)
         pre_args , _ = pre_parser.parse_known_args(argv)
-        config = RootSignerPKIConfig_DEV(pre_args.certificate)
+        config = RootSignerPKIConfig(pre_args.certificate)
         config.handle_pki_file()
         file_defaults = config.get_dn_policies('ca_root.policy', 'intermediate')
         ca_parser = CSRSigningParser(prog="ftwpkicasign")
@@ -165,9 +72,9 @@ def prog_ca_root_signing_DEV(argv: list[str] | None = None) -> int:
         # !SECTION - Passwordhandling
 
         # SECTION - Signing
-        private_key_obj= load_private_key_from_pem(
-            pem_data = config.private_key, 
-            passphrase=pass_phrase)
+        private_key_obj = load_private_key_from_pem(
+            pem_data=config.private_key(temp_key_pem), passphrase=pass_phrase
+        )
         policy = IntermediatePolicy(pathlength = args.path_length)
         validity_days = validate_and_clamp_validity(ca_cert, args.validity_days)
         cert_signer = CertificateSigner(
@@ -210,7 +117,7 @@ def prog_ca_root_signing_DEV(argv: list[str] | None = None) -> int:
     except Exception as e:
         report_error(e)
         return 1
-#!SECTION - prog_ca_root_signing_DEV
+#!SECTION - prog_ca_root_signing
 
 
 
@@ -230,7 +137,7 @@ if __name__ == "__main__":  # pragma: no cover
     test_files = [
         # "get_started_programms.ci.rst",
         "get_started_programms_DEV.ci.rst",
-        # "get_started_run_programms_DEV.ci.rst",
+        "get_started_run_programms_DEV.ci.rst",
         # "get_started_run_programms.ci.rst",
     ]
     for file in test_files:
